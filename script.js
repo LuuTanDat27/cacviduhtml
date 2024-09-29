71abf4d79ff6ea9e47e3cd9b72492d6174bece5e
LuuTanDat27 <115093574+LuuTanDat27@users.noreply.github.com>
2024-09-29 16:18:28 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+document.getElementById('close-btn').addEventListener('click', function() {
+    document.getElementById('notification').style.display = 'none';
+});
+// Mở modal khi trang được tải
+
